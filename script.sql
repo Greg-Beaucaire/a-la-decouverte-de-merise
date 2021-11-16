0bdab5fb71de86ce15e7eae8372557ba50090623
Greg-Beaucaire <greg.beaucaire@gmail.com>
2021-11-16 12:18:33 +0100

    Add files via upload

--- script.sql
+++ script.sql
@@ -1,63 +1,62 @@
-CREATE TABLE Individu(
-   id TEXT,
-   code VARCHAR(250) NOT NULL,
-   nom VARCHAR(50),
-   prénom VARCHAR(50),
-   PRIMARY KEY(id),
+CREATE TABLE individu(
+   id_individu SERIAL,
+   code VARCHAR(250)  NOT NULL,
+   nom VARCHAR(250)  NOT NULL,
+   prénom VARCHAR(250)  NOT NULL,
+   numero VARCHAR(50) ,
+   rue VARCHAR(250) ,
+   ville VARCHAR(250) ,
+   code_postal VARCHAR(50) ,
+   PRIMARY KEY(id_individu),
    UNIQUE(code)
 );
 
-CREATE TABLE Professeur(
-   id TEXT,
-   compétence VARCHAR(50),
-   PRIMARY KEY(id),
-   FOREIGN KEY(id) REFERENCES Individu(id)
+CREATE TABLE professeur(
+   id_individu INTEGER,
+   compétence VARCHAR(250) ,
+   PRIMARY KEY(id_individu),
+   FOREIGN KEY(id_individu) REFERENCES individu(id_individu)
 );
 
-CREATE TABLE Adresse(
-   id VARCHAR(50),
-   numéro VARCHAR(50) NOT NULL,
-   rue VARCHAR(50) NOT NULL,
-   ville VARCHAR(50) NOT NULL,
-   code_postal VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id)
+CREATE TABLE etudiant(
+   id_individu INTEGER,
+   diplome VARCHAR(250) ,
+   PRIMARY KEY(id_individu),
+   FOREIGN KEY(id_individu) REFERENCES individu(id_individu)
 );
 
-CREATE TABLE Matière(
-   id COUNTER,
-   nom VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id),
+CREATE TABLE matiere(
+   id_matiere SERIAL,
+   nom VARCHAR(250)  NOT NULL,
+   PRIMARY KEY(id_matiere),
    UNIQUE(nom)
 );
 
-CREATE TABLE Salle(
-   id COUNTER,
-   nom VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id),
+CREATE TABLE salle(
+   id_salle INTEGER,
+   nom VARCHAR(250)  NOT NULL,
+   PRIMARY KEY(id_salle),
    UNIQUE(nom)
 );
 
-CREATE TABLE Etudiant(
-   id_1 TEXT,
-   diplome VARCHAR(50),
-   id VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_1),
-   UNIQUE(id),
-   FOREIGN KEY(id_1) REFERENCES Individu(id),
-   FOREIGN KEY(id) REFERENCES Adresse(id)
+CREATE TABLE cours(
+   id_salle INTEGER,
+   id SERIAL,
+   heure_de_début TIMESTAMP NOT NULL,
+   heure_de_fin TIMESTAMP NOT NULL,
+   id_matiere INTEGER NOT NULL,
+   id_individu INTEGER NOT NULL,
+   PRIMARY KEY(id_salle, id),
+   FOREIGN KEY(id_salle) REFERENCES salle(id_salle),
+   FOREIGN KEY(id_matiere) REFERENCES matiere(id_matiere),
+   FOREIGN KEY(id_individu) REFERENCES professeur(id_individu)
 );
 
-CREATE TABLE Cours(
-   id_2 INT,
-   id COUNTER,
-   heure_de_début VARCHAR(50) NOT NULL,
-   heure_de_fin VARCHAR(50),
-   id_1 INT NOT NULL,
-   id_3 TEXT NOT NULL,
-   id_4 TEXT NOT NULL,
-   PRIMARY KEY(id_2, id),
-   FOREIGN KEY(id_2) REFERENCES Salle(id),
-   FOREIGN KEY(id_1) REFERENCES Matière(id),
-   FOREIGN KEY(id_3) REFERENCES Etudiant(id_1),
-   FOREIGN KEY(id_4) REFERENCES Professeur(id)
+CREATE TABLE etudiant_cours(
+   id_individu INTEGER,
+   id_salle INTEGER,
+   id INTEGER,
+   PRIMARY KEY(id_individu, id_salle, id),
+   FOREIGN KEY(id_individu) REFERENCES etudiant(id_individu),
+   FOREIGN KEY(id_salle, id) REFERENCES cours(id_salle, id)
 );
