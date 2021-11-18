92671d35846bf70c847c62a3dfee2c2d5453b221
Greg-Beaucaire <greg.beaucaire@gmail.com>
2021-11-18 16:40:40 +0100

    finger crossed

--- script.sql
+++ script.sql
@@ -1,62 +1,59 @@
-CREATE TABLE individu(
-   id_individu SERIAL,
-   code VARCHAR(250)  NOT NULL,
-   nom VARCHAR(250)  NOT NULL,
-   prenom VARCHAR(250)  NOT NULL,
-   numero VARCHAR(50) ,
-   rue VARCHAR(250) ,
-   ville VARCHAR(250) ,
-   code_postal VARCHAR(50) ,
-   PRIMARY KEY(id_individu),
-   UNIQUE(code)
-);
-
 CREATE TABLE professeur(
-   id_individu INTEGER,
+   id_professeur SERIAL,
+   code VARCHAR(25)  NOT NULL,
+   prenom VARCHAR(50)  NOT NULL,
+   nom VARCHAR(50)  NOT NULL,
    competence VARCHAR(250) ,
-   PRIMARY KEY(id_individu),
-   FOREIGN KEY(id_individu) REFERENCES individu(id_individu)
+   CONSTRAINT PK_professeur PRIMARY KEY(id_professeur),
+   CONSTRAINT AK_professeur UNIQUE(code)
 );
 
 CREATE TABLE etudiant(
-   id_individu INTEGER,
-   diplome VARCHAR(250) ,
-   PRIMARY KEY(id_individu),
-   FOREIGN KEY(id_individu) REFERENCES individu(id_individu)
+   id_etudiant SERIAL,
+   code VARCHAR(25)  NOT NULL,
+   nom VARCHAR(25)  NOT NULL,
+   prenom VARCHAR(50)  NOT NULL,
+   numero VARCHAR(250) ,
+   rue VARCHAR(50) ,
+   ville VARCHAR(25) ,
+   code_postal VARCHAR(10) ,
+   diplome VARCHAR(50) ,
+   CONSTRAINT PK_etudiant PRIMARY KEY(id_etudiant),
+   CONSTRAINT AK_etudiant UNIQUE(code)
 );
 
 CREATE TABLE matiere(
    id_matiere SERIAL,
-   nom VARCHAR(250)  NOT NULL,
-   PRIMARY KEY(id_matiere),
-   UNIQUE(nom)
+   nom VARCHAR(50)  NOT NULL,
+   CONSTRAINT PK_matiere PRIMARY KEY(id_matiere),
+   CONSTRAINT AK_matiere UNIQUE(nom)
 );
 
 CREATE TABLE salle(
    id_salle INTEGER,
-   nom VARCHAR(250)  NOT NULL,
-   PRIMARY KEY(id_salle),
-   UNIQUE(nom)
+   nom VARCHAR(50)  NOT NULL,
+   CONSTRAINT PK_salle PRIMARY KEY(id_salle),
+   CONSTRAINT AK_salle UNIQUE(nom)
 );
 
 CREATE TABLE cours(
-   id_salle INTEGER,
-   id SERIAL,
-   heure_de_début TIMESTAMP NOT NULL,
+   id_cours SERIAL,
+   heure_de_debut TIMESTAMP NOT NULL,
    heure_de_fin TIMESTAMP NOT NULL,
    id_matiere INTEGER NOT NULL,
-   id_individu INTEGER NOT NULL,
-   PRIMARY KEY(id_salle, id),
-   FOREIGN KEY(id_salle) REFERENCES salle(id_salle),
-   FOREIGN KEY(id_matiere) REFERENCES matiere(id_matiere),
-   FOREIGN KEY(id_individu) REFERENCES professeur(id_individu)
+   id_salle INTEGER NOT NULL,
+   id_professeur INTEGER NOT NULL,
+   CONSTRAINT PK_cours PRIMARY KEY(id_cours),
+   CONSTRAINT AK_cours_ident UNIQUE(id_salle, heure_de_debut, heure_de_fin),
+   CONSTRAINT FK_cours_matiere FOREIGN KEY(id_matiere) REFERENCES matiere(id_matiere),
+   CONSTRAINT FK_cours_salle FOREIGN KEY(id_salle) REFERENCES salle(id_salle),
+   CONSTRAINT FK_cours_professeur FOREIGN KEY(id_professeur) REFERENCES professeur(id_professeur)
 );
 
-CREATE TABLE etudiant_cours(
-   id_individu INTEGER,
-   id_salle INTEGER,
-   id INTEGER,
-   PRIMARY KEY(id_individu, id_salle, id),
-   FOREIGN KEY(id_individu) REFERENCES etudiant(id_individu),
-   FOREIGN KEY(id_salle, id) REFERENCES cours(id_salle, id)
+CREATE TABLE participant(
+   id_etudiant INTEGER,
+   id_cours INTEGER,
+   CONSTRAINT PK_participant PRIMARY KEY(id_etudiant, id_cours),
+   CONSTRAINT FK_participant_etudiant FOREIGN KEY(id_etudiant) REFERENCES etudiant(id_etudiant),
+   CONSTRAINT FK_participant_cours FOREIGN KEY(id_cours) REFERENCES cours(id_cours)
 );
